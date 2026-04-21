9d3f9b42129c799abc324ca641fc580e9c7bef64
Jose Costa <j.costa.3394@gmail.com>
2026-04-21 12:58:21 +0200

    style: convert arrow functions to traditional function declarations (20th batch)

--- src/modules/MIDI/useCases/midiNoteCrud/__tests__/moveMidiNote.spec.ts
+++ src/modules/MIDI/useCases/midiNoteCrud/__tests__/moveMidiNote.spec.ts
@@ -3,13 +3,15 @@ import { describe, it, expect, beforeEach } from 'vitest';
 import { midiStore } from '../../../stores/midiStore';
 import { moveMidiNote } from '../moveMidiNote';
 
-const note = (id: string) => ({
-    id,
-    pitch: 60,
-    startBeat: 0,
-    duration: 0.25,
-    velocity: 100,
-});
+function note(id: string) {
+    return {
+        id,
+        pitch: 60,
+        startBeat: 0,
+        duration: 0.25,
+        velocity: 100,
+    };
+}
 
 describe('moveMidiNote', () => {
     beforeEach(() => {
