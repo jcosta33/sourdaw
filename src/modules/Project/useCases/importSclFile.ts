a2f8790f0f04021bebf0f63a67fcd414ba5995cb
José Costa <j.costa.3394@gmail.com>
2026-06-29 11:25:38 +0200

    Route SCL parsing through Project repository

--- src/modules/Project/useCases/importSclFile.ts
+++ src/modules/Project/useCases/importSclFile.ts
@@ -1,9 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-restricted-imports -- this IS a repository/adapter layer: invoke bridges native parse_scl command
-import { invoke } from '@tauri-apps/api/core';
-
 import { registerTuningTable } from '#/modules/AudioEngine/useCases';
 import { notifyUser } from '#/utils/Notification/notifyUser';
 
+import { parseScl } from '../repositories/nativeTuning/parseScl';
 import { projectStore } from '../stores/projectStore';
 
 import { pickFiles } from './fileDialog';
@@ -26,13 +24,7 @@ export async function importSclFile(): Promise<void> {
 
         const content = await firstFile.text();
 
-        const result = (await invoke('parse_scl', {
-            content,
-        })) as {
-            name: string;
-            description: string;
-            frequencies: number[];
-        };
+        const result = await parseScl(content);
 
         const project = projectStore.value;
         if (!project) {
