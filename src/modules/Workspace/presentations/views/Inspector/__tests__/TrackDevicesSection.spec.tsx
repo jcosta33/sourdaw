d0040595268ba5f14e089462fd0269f2b1dc81e6
José Costa <j.costa.3394@gmail.com>
2026-07-03 19:44:25 +0200

    test(workspace): reset context menu mocks

--- src/modules/Workspace/presentations/views/Inspector/__tests__/TrackDevicesSection.spec.tsx
+++ src/modules/Workspace/presentations/views/Inspector/__tests__/TrackDevicesSection.spec.tsx
@@ -192,6 +192,9 @@ describe('TrackDevicesSection', () => {
 
     beforeEach(() => {
         vi.clearAllMocks();
+        mockGetPlatformPlugins.mockReturnValue([]);
+        mockGetPluginById.mockReturnValue(null);
+        mockUseStore.mockReturnValue({ scannedPlugins: [] });
     });
 
     it('should render without crashing', () => {
