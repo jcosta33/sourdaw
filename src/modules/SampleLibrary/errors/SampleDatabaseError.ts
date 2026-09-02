b44811b223097dee872d7f4e829bda4504c4db7e
José Costa <j.costa.3394@gmail.com>
2026-09-03 00:29:04 +0200

    refactor(hygiene): remove dead sample database subsystem and demo audio generators

--- src/modules/SampleLibrary/errors/SampleDatabaseError.ts
+++
@@ -1,6 +0,0 @@
-import { createAppError, type AppError } from '#/infra/errors/createAppError';
-
-export type SampleDatabaseError = AppError<'SampleDatabase'>;
-
-export const createSampleDatabaseError = (message: string): SampleDatabaseError =>
-    createAppError('SampleDatabase', message);
