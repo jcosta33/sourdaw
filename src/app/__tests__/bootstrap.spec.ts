90470ac4f21bfa7cd9dd06a0acf514f880552c2f
José Costa <j.costa.3394@gmail.com>
2026-09-10 04:30:39 +0200

    test(app): register the yeast handler map in the bootstrap spec (#2111)

--- src/app/__tests__/bootstrap.spec.ts
+++ src/app/__tests__/bootstrap.spec.ts
@@ -530,6 +530,7 @@ vi.mock('#/modules/Yeast/stores', () => ({ setYeastEventBus: noop }));
 vi.mock('#/modules/Yeast/useCases', () => ({
     configureYeastRuntime: noop,
     createOfflineYeastMidiProcessor: noop,
+    getYeastHandlers: sentinelHandlers('Yeast'),
     processRealtimeMidiInput: noop,
     teardownYeastRuntime: noop,
 }));
@@ -622,6 +623,7 @@ describe('bootstrap', () => {
         'WebMidiInput',
         'Rave',
         'ControlRoom',
+        'Yeast',
     ];
 
     it('validates LLM strategy names against the command catalogue before handler registration', () => {
